edde7e89e8bb677ce35777637eee4bfa93bd2b17
Antigen-1 <zhanghao@antigen.top>
2022-10-27 16:34:27 +0800

    repair `get-argb-pixels`

--- main.rkt
+++ main.rkt
@@ -1,15 +1,14 @@
 #lang racket
 (require racket/draw)
-(provide (all-from-out racket/draw) make-ascii-pic ascii-table)
+(provide (all-from-out racket/draw) make-ascii-pic ascii-table simply-make-ascii-pic)
 
 (define/contract ascii-table (parameter/c bytes?) (make-parameter #"abcdefghijklmnopqrstuvwxyz!@#$%^&*()-_ "))
 
-(define get-pixel
+(define get-pixels
   (lambda (x y w h bitmap)
-    (let ((color (make-bytes 4)))
+    (let ((color (make-bytes (* 4 w h))))
       (send bitmap get-argb-pixels x y w h color)
-      ;;ignore the alpha value
-      (subbytes color 1))))
+      color)))
 
 (define get-grey-value
   (lambda (bytes)
@@ -24,13 +23,17 @@
         (bytes-ref t (exact-floor (/ (get-grey-value bytes) gap)))))))
 
 (define/contract make-ascii-pic
+  (-> exact-nonnegative-integer? exact-nonnegative-integer? exact-nonnegative-integer? exact-nonnegative-integer? (is-a?/c bitmap%) bytes?)
+  (lambda (start-x start-y width height bitmap)
+    (let ((proc (create-handler))
+          (pixels (get-pixels start-x start-y width height bitmap)))
+      (let loop ((pixels pixels) (x 0) (byte-list null))
+        (cond
+          ((bytes=? pixels #"") (list->bytes (reverse byte-list)))
+          ((> x width) (loop pixels 0 (cons 10 byte-list)))
+          (else (loop (subbytes pixels 4) (add1 x) (cons (proc (subbytes pixels 1 4)) byte-list))))))))
+
+(define/contract simply-make-ascii-pic
   (-> (is-a?/c bitmap%) bytes?)
   (lambda (bitmap)
-    (let ((w (send bitmap get-width))
-          (h (send bitmap get-height))
-          (p (create-handler)))
-      (let loop ((x 0) (y 0) (byte-list null))
-        (cond
-          ((> y h) (list->bytes (reverse byte-list)))
-          ((> x w) (loop 0 (add1 y) (cons 10 byte-list)))
-          (else (loop (add1 x) y (cons (p (get-pixel x y w h bitmap)) byte-list))))))))
+    (make-ascii-pic 0 0 (send bitmap get-width) (send bitmap get-height) bitmap)))
